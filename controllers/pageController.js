ab51d64676fa285bd18d535b1cde79b051d8cddb
mikejuwon <mikejuwon737@gmail.com>
2022-12-02 13:08:41 +0100

    new changes

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const moment = require("moment");
+const { Op } = require("sequelize");
 const db = require("../config/database");
 const Gig = require("../models/Gig");
 
@@ -14,7 +16,14 @@ const homePage = async (req, res) => {
 // Add Gig Page
 const addGigPage = async (req, res) => {
     try {
-        res.render("add");
+        res.render("add", {
+            errors: [],
+            title: "",
+            technologies: "",
+            budget: "",
+            description: null,
+            contact_email: ""
+        });
     } catch (err) {
         console.log(err);
     }
@@ -23,15 +32,53 @@ const addGigPage = async (req, res) => {
 // Add Gig
 const addGig = async (req, res) => {
     try {
-        const { title, technologies, budget, description, contact_email } = req.body;
-        await Gig.create({
-            title,
-            technologies,
-            description,
-            budget,
-            contact_email
-        });
-        return res.redirect("/all-gigs");
+        let { title, technologies, budget, description, contact_email } = req.body;
+        let errors = [];
+
+        // Validate Fields
+        if (!title) {
+            errors.push({ text: "Please add a title" });
+        }
+        if (!technologies) {
+            errors.push({ text: "Please add some technologies" });
+        }
+        if (!description) {
+            errors.push({ text: "Please add a description" });
+        }
+        if (!contact_email) {
+            errors.push({ text: "Please add a contact email" });
+        }
+
+        // Check for errors
+        if (errors.length > 0) {
+            res.render("add", {
+                errors,
+                title,
+                technologies,
+                budget,
+                description,
+                contact_email
+            });
+        } else {
+            if (!budget) {
+                budget = "Unknown";
+            } else {
+                budget = `₦${budget}`;
+            }
+
+            // Make lowercase and remove space after comma
+            technologies = technologies.toLowerCase().replace(/, /g, ",");
+
+            // Insert into table
+            await Gig.create({
+                title,
+                technologies,
+                description,
+                budget,
+                contact_email
+            });
+            return res.redirect("/all-gigs/1");
+        }
     } catch (err) {
         console.log(err);
     }
@@ -40,19 +87,121 @@ const addGig = async (req, res) => {
 // Get gig list
 const getGigs = async (req, res) => {
   try {
+    // pagination
+    const page = req.query.page || req.params.page || 1;
+    const limit = req.query.limit || 3;
+    const startIndex = (page - 1) * limit;
+    const currentPage = page;
     // Get all gigs from database and sort by date in descending order
     const gigs = await Gig.findAll({
-        order:  [["id", "ASC"]]
+        order: [["createdAt", "DESC"]],
+        offset: startIndex,
+        limit: limit
     });
-    res.render("gigs", { gigs });
+
+    // Get total number of gigs
+    const totalGigs = await Gig.count();
+    
+    res.render("gigs", { gigs, moment, current: currentPage, pages: Math.ceil(totalGigs / limit), result: null, term: null });
   } catch (error) {
     console.log(error);
   }
+};
+
+const searchGigs = async (req, res) => {
+    try {
+        // pagination
+        const page = req.query.page || req.params.page || 1;
+        const limit = req.query.limit || 3;
+        const startIndex = (page - 1) * limit;
+        const currentPage = page;
+
+        let { term } = req.query;
+        term = term.toLowerCase();
+
+        const gigs = await Gig.findAll({
+            where: {
+                technologies: {
+                    [Op.like]: "%" + term + "%"
+                }
+            },
+            order: [["createdAt", "DESC"]],
+            offset: startIndex,
+            limit: limit
+        });
+
+        const totalGigs = await Gig.count({
+            where: {
+                technologies: {
+                    [Op.like]: "%" + term + "%"
+                }
+            }
+        });
+        res.render("gigs", { gigs, moment, current: currentPage, pages: Math.ceil(totalGigs / limit), term, result: totalGigs });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const updateGig = async (req, res) => {
+    try {
+        let { title, technologies, budget, description, contact_email } = req.body;
+        let errors = [];
+
+        // Validate Fields
+        if (!title) {
+            errors.push({ text: "Please add a title" });
+        }
+        if (!technologies) {
+            errors.push({ text: "Please add some technologies" });
+        }
+        if (!description) {
+            errors.push({ text: "Please add a description" });
+        }
+        if (!contact_email) {
+            errors.push({ text: "Please add a contact email" });
+        }
+
+        // Check for errors
+        if (errors.length > 0) {
+            res.render("edit", {
+                errors,
+                title,
+                technologies,
+                budget,
+                description,
+                contact_email
+            });
+        } else {
+            if (!budget) {
+                budget = "Unknown";
+            } else {
+                budget = `₦${budget}`;
+            }
+
+            // Make lowercase and remove space after comma
+            technologies = technologies.toLowerCase().replace(/, /g, ",");
+            const gig = await Gig.findOne({ where: { id: req.params.id } });
+            // Insert into table
+            await gig.update({
+                title,
+                technologies,
+                description,
+                budget,
+                contact_email
+            });
+            return res.redirect("/all-gigs/1");
+        }
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 module.exports = {
     homePage,
     addGigPage,
     addGig,
-    getGigs
+    getGigs,
+    searchGigs,
+    updateGig
 };
--- routes/pages.js
+++ routes/pages.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
-const { homePage, addGigPage, addGig, getGigs } = require('../controllers/pageController');
+const { homePage, addGigPage, addGig, getGigs, searchGigs } = require('../controllers/pageController');
 
 router.get("/", homePage);
 router.get("/add-gig", addGigPage);
 router.post("/add-gig", addGig);
-router.get("/all-gigs", getGigs);
+router.get("/all-gigs/:page", getGigs);
+router.get("/search-gigs", searchGigs);
 
 module.exports = router;
